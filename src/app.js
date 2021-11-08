9d42e427a18e980b434ed940c2ad866d046ef679
Kécia Santos <keciakaroline@gmail.com>
2021-11-08 16:17:35 +0100

    Updated forecast API

--- src/app.js
+++ src/app.js
@@ -92,25 +92,36 @@ function showFahrenheitTemperature(event) {
   tempElement.innerHTML = Math.round(fahrenheitTemp);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function showForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-    <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
-      <img src="http://openweathermap.org/img/wn/04n@2x.png" alt="" width="50" />
-      <div class="weather-forecast-temperatures">
-        <strong>18°C</strong> <br />
-        <i class="fas fa-long-arrow-alt-down"></i> <br />
-        16°C
-      </div>
-    </div>`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="" width="50" />
+        <div class="weather-forecast-temperatures">
+          <strong>${Math.round(forecastDay.temp.max)}</strong> <br />
+          <i class="fas fa-long-arrow-alt-down"></i> <br />
+          ${Math.round(forecastDay.temp.min)}
+        </div>
+      </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
