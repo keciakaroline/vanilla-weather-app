5adda9115a5d9580c0990a024c781ee0834fa74b
Kécia Santos <keciakaroline@gmail.com>
2021-11-08 15:36:14 +0100

    Added API for lat & long

--- src/app.js
+++ src/app.js
@@ -50,6 +50,16 @@ function showWeather(response) {
   document
     .querySelector("#icon")
     .setAttribute("alt", `${response.data.weather[0].description}`);
+
+  getForecast(response.data.coord);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "ce9e9a1384d8ee7b166d7542086e2fdc";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showForecast);
 }
 
 function search(city) {
@@ -82,7 +92,9 @@ function showFahrenheitTemperature(event) {
   tempElement.innerHTML = Math.round(fahrenheitTemp);
 }
 
-function showForecast() {
+function showForecast(response) {
+  console.log(response.data.daily);
+
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -115,4 +127,3 @@ fahrenheitElement.addEventListener("click", showFahrenheitTemperature);
 document.querySelector("#search-form").addEventListener("submit", handleForm);
 
 search("Bochum");
-showForecast();
